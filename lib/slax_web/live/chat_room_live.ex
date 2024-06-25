8b0fbdfc954b6b5a419318dfb28c1e803ae45020
Mark Szymczyk <mark@meandmark.com>
2024-06-25 18:36:12 -0400

    Use @ syntax in template.

--- lib/slax_web/live/chat_room_live.ex
+++ lib/slax_web/live/chat_room_live.ex
@@ -10,9 +10,9 @@ defmodule SlaxWeb.ChatRoomLive do
     	<div class="flex justify-between items-center flex-shrint-0 h-16 bg-white border-b border-slate-300 px-4">
     		<div class="flex flext-col gap-1.5">
     			<h1 class="text-sm font-bold leading-none">
-    				#<%= assigns.room.name %>
+    				#<%= @room.name %>
     			</h1>
-    			<div class="text-xs leading-none h-3.5"><%= assigns.room.topic %></div>
+    			<div class="text-xs leading-none h-3.5"><%= @room.topic %></div>
     		</div>
     	</div>
     </div>
