d094664d47eb8ac391b04574991d564e817e6750
Mark Szymczyk <mark@meandmark.com>
2024-06-25 18:34:45 -0400

    Show real room and topic
    Change the mount function to return the first room in the database.
    
    Use the room name and topic as the text to show on the page.

--- lib/slax_web/live/chat_room_live.ex
+++ lib/slax_web/live/chat_room_live.ex
@@ -1,15 +1,18 @@
 defmodule SlaxWeb.ChatRoomLive do
   use SlaxWeb, :live_view
   
+  alias Slax.Repo
+  alias Slax.Chat.Room
+  
   def render(assigns) do
     ~H"""
     <div class ="flext flex-col flex-grow shadow-lg">
     	<div class="flex justify-between items-center flex-shrint-0 h-16 bg-white border-b border-slate-300 px-4">
     		<div class="flex flext-col gap-1.5">
     			<h1 class="text-sm font-bold leading-none">
-    				#room-name
+    				#<%= assigns.room.name %>
     			</h1>
-    			<div class="text-xs leading-none h-3.5">Placeholder topic</div>
+    			<div class="text-xs leading-none h-3.5"><%= assigns.room.topic %></div>
     		</div>
     	</div>
     </div>
@@ -17,7 +20,11 @@ defmodule SlaxWeb.ChatRoomLive do
   end
   
   def mount(_params, _session, socket) do
-    {:ok, socket}
+  	room = Room
+  	|> Repo.all()
+		|> List.first()
+    
+  	{:ok, assign(socket, :room, room)}
   end
   
 end
