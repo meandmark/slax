6fc5ff6c493518c6c621612ff23d28532fb3b887
Mark Szymczyk <mark@meandmark.com>
2024-06-25 18:08:44 -0400

    Come up with the initial view.

--- lib/slax_web/live/chat_room_live.ex
+++ lib/slax_web/live/chat_room_live.ex
@@ -3,7 +3,16 @@ defmodule SlaxWeb.ChatRoomLive do
   
   def render(assigns) do
     ~H"""
-    <div>Welcome to the chat!</div>
+    <div class ="flext flex-col flex-grow shadow-lg">
+    	<div class="flex justify-between items-center flex-shrint-0 h-16 bg-white border-b border-slate-300 px-4">
+    		<div class="flex flext-col gap-1.5">
+    			<h1 class="text-sm font-bold leading-none">
+    				#room-name
+    			</h1>
+    			<div class="text-xs leading-none h-3.5">Placeholder topic</div>
+    		</div>
+    	</div>
+    </div>
     """
   end
   
