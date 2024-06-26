1412ecf7dbe34127a798ee9724276d63d971ba03
Mark Szymczyk <mark@meandmark.com>
2024-06-26 14:41:49 -0400

    Add a sidebar of rooms
    Adding the sidebar with a list of rooms involves doing the following:
    
    Changing the mount function to show a list of rooms.
    
    Adding the sidebar to the render function.
    
    Adding a room_link private function.
    
    Problem: the room appears below the sidebar instead of to the right of the sidebar.

--- lib/slax_web/live/chat_room_live.ex
+++ lib/slax_web/live/chat_room_live.ex
@@ -6,6 +6,24 @@ defmodule SlaxWeb.ChatRoomLive do
   
   def render(assigns) do
     ~H"""
+    <div class="flex flex-col flex-shrink-0 w-64 bg-slate-100">
+      <div class="flex justify-between items-center flex-shrink-0 h-16 border-b border-slate-300 px-4">
+        <div class="flex flex-col gap-1.5">
+          <h1 class="text-lg font-bold text-gray-800">
+            Slax
+          </h1>
+        </div>
+      </div>
+      <div class="mt-4 overflow-auto">
+        <div class="flex items-center h-8 px-3 group">
+          <span class="ml-2 leading-none font-medium text-sm">Rooms</span>
+        </div>
+        <div id="rooms-list">
+          <.room_link :for={room <- @rooms} room={room} active={room.id == @room.id} />
+        </div>
+      </div>
+    </div>
+    
     <div class ="flext flex-col flex-grow shadow-lg">
     	<div class="flex justify-between items-center flex-shrint-0 h-16 bg-white border-b border-slate-300 px-4">
     		<div class="flex flext-col gap-1.5">
@@ -25,13 +43,28 @@ defmodule SlaxWeb.ChatRoomLive do
     """
   end
   
+  defp room_link(assigns) do
+    ~H"""
+    <a
+      class={[
+       "flex items-center h-8 text-sm pl-8 pr-3",
+        (@active && "bg-slate-300") || "hover:bg-slate-300"
+      ]}
+      href="#"
+    >
+      <.icon name="hero-hashtag" class="h-4 w-4" />
+      <span class={["ml-2 leading-none", @active && "font-bold"]}>
+        <%= @room.name %>
+      </span>
+    </a>
+    """
+  end
+  
   def mount(_params, _session, socket) do
-  	# This room variable is passed to the render function.
-  	room = Room
-  	|> Repo.all()
-		|> List.first()
-    
-  	{:ok, assign(socket, hide_topic?: false, room: room)}
+  	rooms = Repo.all(Room)
+  	room = List.first(rooms)
+  	
+  	{:ok, assign(socket, hide_topic?: false, room: room, rooms: rooms)}
   end
   
   def handle_event("toggle-topic", _params, socket) do
