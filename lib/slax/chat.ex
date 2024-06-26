fd0f4f63b3fd8711916a65a36b9bb36ff6b96fb6
Mark Szymczyk <mark@meandmark.com>
2024-06-26 17:02:54 -0400

    Add functions to list the rooms, get the first room, and get a room from its id.

---
+++ lib/slax/chat.ex
@@ -0,0 +1,19 @@
+defmodule Slax.Chat do
+  alias Slax.Chat.Room
+  alias Slax.Repo
+  
+  def get_first_room! do
+    [room | _] = list_rooms()
+    room
+  end
+  
+  def get_room!(id) do
+  	Repo.get!(Room, id)
+  end
+  
+  def list_rooms do
+    Room
+    |> Repo.all()
+  end
+  
+end
