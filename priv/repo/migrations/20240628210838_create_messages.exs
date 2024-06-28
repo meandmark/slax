ccfdd334902e2b34da216835276be29fb4895aaa
Mark Szymczyk <mark@meandmark.com>
2024-06-28 17:10:06 -0400

    Create migration for supporting messages.

---
+++ priv/repo/migrations/20240628210838_create_messages.exs
@@ -0,0 +1,13 @@
+defmodule Slax.Repo.Migrations.CreateMessages do
+  use Ecto.Migration
+
+  def change do
+    create table(:messages) do
+      add :user_id, references(:users, on_delete: :nothing)
+
+      timestamps(type: :utc_datetime)
+    end
+
+    create index(:messages, [:user_id])
+  end
+end
