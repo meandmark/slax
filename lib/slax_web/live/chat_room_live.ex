094a7e0328598a2c743f00ff0404f155ab75abdc
Mark Szymczyk <mark@meandmark.com>
2024-07-02 14:42:21 -0400

    Update the UI to show the correct time zone.
    This involves messing around with the HEEx templates and updating the message_timestamp function to use the local time zone.

--- lib/slax_web/live/chat_room_live.ex
+++ lib/slax_web/live/chat_room_live.ex
@@ -90,7 +90,12 @@ defmodule SlaxWeb.ChatRoomLive do
 
     	</div>
     	<div id="room-messages" class="flex flex-col flex-grow overflow-auto" phx-update="stream">
-        <.message :for={{dom_id, message} <- @streams.messages} dom_id={dom_id} message={message} />
+        <.message
+          :for={{dom_id, message} <- @streams.messages}
+          dom_id={dom_id}
+          message={message}
+          timezone={@timezone}
+        />
      </div>
      
      <div class="h-12 bg-white px-4 pb-4">
@@ -121,6 +126,7 @@ defmodule SlaxWeb.ChatRoomLive do
   
   attr :dom_id, :string, required: true
   attr :message, Message, required: true
+  attr :timezone, :string, required: true
   
   defp message(assigns) do
    ~H"""
@@ -131,7 +137,9 @@ defmodule SlaxWeb.ChatRoomLive do
          <.link class="text-sm font-semibold hover:underline">
         	<span><%= username(@message.user) %></span>
          </.link>
-         <span class="ml-1 text-xs text-gray-500"><%= message_timestamp(@message) %></span>
+         <span :if={@timezone} class="ml-1 text-xs text-gray-500">
+            <%= message_timestamp(@message, @timezone) %>
+          </span>
          <p class="text-sm"><%= @message.body %></p>
        </div>
      </div>
@@ -146,9 +154,9 @@ defmodule SlaxWeb.ChatRoomLive do
 	  |> String.capitalize()
 	end
 	
-	defp message_timestamp(message) do
+	defp message_timestamp(message, timezone) do
 	  message.inserted_at
-	  |> Timex.Timezone.convert(Timex.Timezone.local())
+	  |> Timex.Timezone.convert(timezone)
 	  |> Timex.format!("%-l:%M %p", :strftime)
 	end
 	
