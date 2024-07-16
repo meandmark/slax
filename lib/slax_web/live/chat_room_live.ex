188284e187bc33a72d24af0c27d9d86de3540477
Mark Szymczyk <mark@meandmark.com>
2024-07-16 17:26:31 -0400

    Add a Delete button to each message in the chat room.

--- lib/slax_web/live/chat_room_live.ex
+++ lib/slax_web/live/chat_room_live.ex
@@ -93,6 +93,7 @@ defmodule SlaxWeb.ChatRoomLive do
     	<div id="room-messages" class="flex flex-col flex-grow overflow-auto" phx-update="stream">
         <.message
           :for={{dom_id, message} <- @streams.messages}
+          current_user={@current_user}
           dom_id={dom_id}
           message={message}
           timezone={@timezone}
@@ -125,6 +126,7 @@ defmodule SlaxWeb.ChatRoomLive do
     """
   end
   
+  attr :current_user, User, required: true
   attr :dom_id, :string, required: true
   attr :message, Message, required: true
   attr :timezone, :string, required: true
@@ -132,6 +134,12 @@ defmodule SlaxWeb.ChatRoomLive do
   defp message(assigns) do
    ~H"""
    <div id={@dom_id} class="relative flex px-4 py-3">
+     <button
+        :if={@current_user.id == @message.user_id}
+        class="absolute top-4 right-4 text-red-500 hover:text-red-800 cursor-pointer"
+      >
+        <.icon name="hero-trash" class="h-4 w-4" />
+      </button>
      <div class="h-10 w-10 rounded flex-shrink-0 bg-slate-300"></div>
      <div class="ml-2">
        <div class="-mt-1">
