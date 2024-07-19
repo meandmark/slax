b2e7e162141907df67655c4f271dfd7f06459f4a
Mark Szymczyk <mark@meandmark.com>
2024-07-19 17:37:07 -0400

    Fix an error when checking whether the key is the Enter key. I need to add a third = character.

--- assets/js/hooks/ChatMessageTextarea.js
+++ assets/js/hooks/ChatMessageTextarea.js
@@ -1,7 +1,7 @@
 const ChatMessageTextarea = {
 	mounted() {
 		this.el.addEventListener('keydown', e => {
-			if (e.key == 'Enter' && !e.shiftKey) {
+			if (e.key === 'Enter' && !e.shiftKey) {
 				const form = document.getElementById("new-message-form");
 				
 				form.dispatchEvent(new Event("change", {bubbles: true, cancelable: true}));
