5390d6a64d43923de66fe3848ba31954820f3120
Mark Szymczyk <mark@meandmark.com>
2024-07-19 15:17:59 -0400

    Register the chat message text area hook.

--- assets/js/app.js
+++ assets/js/app.js
@@ -21,9 +21,11 @@ import "phoenix_html"
 import {Socket} from "phoenix"
 import {LiveSocket} from "phoenix_live_view"
 import topbar from "../vendor/topbar"
+import ChatMessageTextarea from ".hooks/ChatMessageTextarea"
 import RoomMessages from "./hooks/RoomMessages"
 
 const hooks = {
+	ChatMessageTextarea
 	RoomMessages,
 }
 
