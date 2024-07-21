fdfcc26b2b8e8187f4ed378f17218ecbd1d537f1
Mark Szymczyk <mark@meandmark.com>
2024-07-21 13:57:11 -0400

    Add a list of users to the sidebar.

--- lib/slax_web/live/chat_room_live.ex
+++ lib/slax_web/live/chat_room_live.ex
@@ -1,6 +1,7 @@
 defmodule SlaxWeb.ChatRoomLive do
   use SlaxWeb, :live_view
   
+  alias Slax.Accounts
   alias Slax.Accounts.User
   alias Slax.Chat
   alias Slax.Chat.{Message, Room}
@@ -21,6 +22,19 @@ defmodule SlaxWeb.ChatRoomLive do
         </div>
         <div id="rooms-list">
           <.room_link :for={room <- @rooms} room={room} active={room.id == @room.id} />
+        </div>
+        <div class="mt-4">
+          <div class="flex items-center h-8 px-3 group">
+            <div class="flex items-center flex-grow focus:outline-none">
+              <span class="ml-2 leading-none font-medium text-sm">Users</span>
+            </div>
+          </div>
+          <div id="users-list">
+            <.user
+              :for={user <- @users}
+              user={user}
+            />
+          </div>
         </div>
       </div>
     </div>
@@ -166,6 +180,24 @@ defmodule SlaxWeb.ChatRoomLive do
    """
 	end
 	  
+	attr :user, User, required: true
+	attr :online, :boolean, default: false
+	
+	defp user(assigns) do
+    ~H"""
+    <.link class="flex items-center h-8 hover:bg-gray-300 text-sm pl-8 pr-3" href="#">
+      <div class="flex justify-center w-4">
+        <%= if @online do %>
+          <span class="w-2 h-2 rounded-full bg-blue-500"></span>
+        <% else %>
+          <span class="w-2 h-2 rounded-full border-2 border-gray-500"></span>
+        <% end %>
+      </div>
+      <span class="ml-2 leading-none"><%= username(@user) %></span>
+    </.link>
+    """
+  end
+	
 	defp username(user) do
 	  user.email
 	  |> String.split("@")
@@ -201,8 +233,14 @@ defmodule SlaxWeb.ChatRoomLive do
   
   def mount(_params, _session, socket) do
   	rooms = Chat.list_rooms()
+  	users = Accounts.list_users()
   	timezone = get_connect_params(socket)["timezone"]
-  	{:ok, assign(socket, rooms: rooms, timezone: timezone)}
+  	
+  	socket =
+  		socket
+  		|> assign(rooms: rooms, timezone: timezone, users: users)
+  		
+  	{:ok, socket}
   end
   
   def handle_params(params, _session, socket) do
