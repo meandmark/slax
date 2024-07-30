ef59c4017ad8e8b7c15f1891f1522549860d5ba9
Mark Szymczyk <mark@meandmark.com>
2024-07-30 14:36:05 -0400

    Add the render, mount, and handle_event functions.

---
+++ lib/slax_web/live/chat_room_live/index.ex
@@ -0,0 +1,50 @@
+defmodule SlaxWeb.ChatRoomLive.Index do
+  use SlaxWeb, :live_view
+
+  alias Slax.Chat
+
+  def render(assigns) do
+    ~H"""
+    <main class="flex-1 p-6 max-w-4xl mx-auto">
+      <div class="mb-4">
+        <h1 class="text-xl font-semibold"><%= @page_title %></h1>
+      </div>
+      <div class="bg-slate-50 border rounded">
+        <div id="rooms" class="divide-y" phx-update="stream">
+          <div
+            :for={{id, room} <- @streams.rooms}
+            class="cursor-pointer p-4 flex justify-between items-center group first:rounded-t last:rounded-b"
+            id={id}
+            phx-click="view-room"
+            phx-value-id={room.id}
+          >
+            <div>
+              <div class="font-medium mb-1">
+                #<%= room.name %>
+                <span class="mx-1 text-gray-500 font-light text-sm opacity-0 group-hover:opacity-100">
+                  View room
+                </span>
+              </div>
+              <div class="text-gray-500 text-sm">
+                <%= if room.topic do %>
+                  <%= room.topic %>
+                <% end %>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </main>
+    """
+  end
+
+  def mount(_params, _session, socket) do
+    rooms = Chat.list_rooms()
+    socket = socket |> assign(page_title: "All rooms") |> stream(:rooms, rooms)
+    {:ok, socket}
+  end
+
+  def handle_event("view-room", %{"id" => id}, socket) do
+    {:noreply, push_navigate(socket, to: ~p"/rooms/#{id}")}
+  end
+end
